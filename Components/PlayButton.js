f1780c486bceaa4eb3368c27d200ef8204d05c97
Amir Sigari <asigari0711@gmail.com>
2020-01-11 14:26:38 -0500

    play buttton 2

--- Components/PlayButton.js
+++ Components/PlayButton.js
@@ -1,2 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import Play_Circle_Filled from 'mdi-react/paly_circle_filled'
+
+const PlayButton = () =>{
+    
+    return (
+        <div>
+            <Play_Circle_Filled/>
+        </div>
+
+    )
+
+}
+
 
